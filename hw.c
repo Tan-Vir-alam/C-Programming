a7afcbc102b93ec73cd248307d337eb43b3d9229
Tan-Vir-alam <tanviralam750738@gmail.com>
2025-03-04 03:40:07 +0600

    Add my assignment.

---
+++ hw.c
@@ -0,0 +1,75 @@
+/*Name: Tanvir Alam.
+My assignment*/
+#include <stdio.h>
+
+int main() {
+
+    printf("Enter chips price:\n");
+     int x;
+    scanf("%d",&x);
+    printf("Enter chocolate price:\n");
+    int y;
+    scanf("%d",&y);
+     int z = x + y;
+    printf("Enter students number:");
+    int a;
+    scanf("%d",&a);
+    int p = z*a;
+    printf("%d",p);
+
+    return 0;
+}
+
+//Problem No:02
+
+
+#include <stdio.h>
+
+int main() {
+
+   printf("Enter time in seconds:");
+   int a;
+   scanf("%d",&a);
+   int hour = a/3600;
+   int min = (a%3600)/60;
+   int sec = a - (hour*3600) - (min*60);
+   printf("%d : %d : %d",hour,min,sec);
+
+    return 0;
+}
+
+//Problem No:03(a)
+
+#include <stdio.h>
+
+int main(){
+printf("      *      \n");
+printf("     * *     \n");
+printf("    * * *    \n");
+printf("   * * * *   \n");
+printf("  * * * * *  \n");
+printf(" * * * * * * \n");
+}
+
+
+//problem No:03(b)
+
+
+#include <stdio.h>
+
+int main() {
+
+   printf("         *          \n");
+   printf("       * * *        \n");
+   printf("     * * * * *      \n");
+   printf("   * * * * * * *    \n");
+   printf(" * * * * * * * * *  \n");
+   printf(" * * *       * * *  \n");
+   printf(" * * *       * * *  \n");
+   printf(" * * *       * * *  \n");
+
+
+
+
+    return 0;
+}
