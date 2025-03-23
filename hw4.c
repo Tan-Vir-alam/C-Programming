5a9ac36b7df1c777f715b423184cc4900eaa0f01
Tanvir Alam <tanviralam750738@gmail.com>
2025-03-24 00:34:47 +0600

    Update hw4.c

--- hw4.c
+++ hw4.c
@@ -36,9 +36,6 @@ int main(){
     else {
     printf("NOT FOUND!");
     }
-
-
-
 return 0;
 }
 
@@ -56,35 +53,3 @@ int main(){
 
 return 0;
 }
-
-//Problem No: 03.
-
-#include <stdio.h>
-int main(){
-  int a;
-  float t,f,c;
-  char b;
-
-  printf("If you want to convert C to F type 1.\n If you want to convert F to C type 2.");
-  scanf("%d",&a);
-
-  printf("Enter your temperature with C or F:");
-
-  scanf("%f,%c",&t,&b);
-  //sacnf("%c",&b);
-
-  if (a==1){
-    f = ((9*t)/5)+32;
-    printf("%.1f Fahrenheit\n",f);
-  }
-  else if (a==2){
-
-    c = ((t-32)*5)/9;
-    printf("%.1f Celsius",c);
-  }
-  else{
-    printf("The choice is not correct.");
-  }
-return 0;
-}
-
