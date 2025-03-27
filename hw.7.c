9a1e0cdce3add4de536b9f1fdddadf6e414f95da
Tanvir Alam <tanviralam750738@gmail.com>
2025-03-27 12:11:43 +0600

    Update hw.7.c

--- hw.7.c
+++ hw.7.c
@@ -18,12 +18,8 @@ int main(){
  printf("++X = %d\n",++num);
  printf("X-- = %d\n"num--);
  printf("--X = %d\n",--num);
-
  getch();
-
 }
-
-
 /*Problem: A little girl has been stuck in a forest. To escape from this forest she can only choose
 those roads which road numbers are divisible by 3 or 5.
 INPUT FFORMAT: Find if the number is divisible by 3,5 or not.
@@ -39,40 +35,5 @@ int main(){
  else{
     printf("Wrong Road.");
  }
-
  getch();
-}
-/*Problem: Lily is so tensed about her weight gain. She's going for a diet, that's why she has to calculate
-her BMI every week. Help her in BMI determination.
-Underweight:- BMI < 18.5
-Normal weight:- 18.5 <= BMI < 24.9
-Overweight:- 25 <= BMI < 29.9
-Obese:- BMI >= 30
-
-INPUT FORMAT: Take weight & height as kg & m.
-OUTPUT FORMAT: Tell her in which condition she is in.*/
-
-#include<stdio.h>
-int main(){
-float weight,height,BMI;
-printf("Enter your weight:");
-scanf("%f",&weight);
-printf("Enter your height:");
-scanf("%f",&height);
-
-BMI = weight/(height*height);
-    if (BMI<18.5){
-    printf("Underweight.");
-    }
-    else if (18.4 < BMI && BMI < 24.5){
-        printf("Normal weight.");
-    }
-    else if (24.9 < BMI && BMI < 29.9){
-        printf("Overweight");
-    }
-    else{
-        printf("Obese");
-    }
-getch();
-
 }
