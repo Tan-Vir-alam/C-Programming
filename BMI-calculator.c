04b5494ff5c303d4c315a2e4cc73a8629ae5206c
Tanvir Alam <tanviralam750738@gmail.com>
2025-03-27 12:15:37 +0600

    Create BMI-Calculator.c
    It's a BMI calculator. I make it for my assignment. Here you can calculate your BMI by using your weight & height.

---
+++ BMI-calculator.c
@@ -0,0 +1,33 @@
+/*Problem: Lily is so tensed about her weight gain. She's going for a diet, that's why she has to calculate
+her BMI every week. Help her in BMI determination.
+Underweight:- BMI < 18.5
+Normal weight:- 18.5 <= BMI < 24.9
+Overweight:- 25 <= BMI < 29.9
+Obese:- BMI >= 30
+INPUT FORMAT: Take weight & height as kg & m.
+OUTPUT FORMAT: Tell her in which condition she is in.*/
+
+#include<stdio.h>
+int main(){
+float weight,height,BMI;
+printf("Enter your weight:");
+scanf("%f",&weight);
+printf("Enter your height:");
+scanf("%f",&height);
+BMI = weight/(height*height);
+  
+    if (BMI<18.5){
+    printf("Underweight.");
+    }
+    else if (18.4 < BMI && BMI < 24.5){
+        printf("Normal weight.");
+    }
+    else if (24.9 < BMI && BMI < 29.9){
+        printf("Overweight");
+    }
+    else{
+        printf("Obese");
+    }
+getch();
+
+}
