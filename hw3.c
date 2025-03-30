fb48cc07dbed8ea98c604302684a62e42d296ef9
Tanvir Alam <tanviralam750738@gmail.com>
2025-03-30 23:49:22 +0600

    hw3.c

--- hw3.c
+++ hw3.c
@@ -1,9 +1,4 @@
-//Name: Tanvir Alam
-//Roll:2401030
-//Problem No:01
-
 #include <stdio.h>
-
 int main(){
     printf("(I) ~\\\/\\\/^%%\\\\\"?\" ~\n(II) \"#b0yc0tt Chatgpt\"\n(III) \"return0;\"\n(IV) \\\\Shiray Shiray Rokto amra Nirob Bhai er vokt0!!^_^\n(V) /\\/\\0TI\\//\\T!0/\\/??\n");
 return 0;
@@ -11,41 +6,30 @@ return 0;
 
 //Problem No:02
 #include <stdio.h>
-
 int main(){
     int f,l,A,F,L;
-
     printf("Enter 1st variable:");
     scanf("%d",&f);
     printf("Enter 2nd variable:");
     scanf("%d",&l);
-
     f = f+l;
     l = f-l;
     f = f-l;
-
     A = f+l;
     F = A-l;
     L = A-f;
-
-
     printf("swap using 3rd variable is: %d,%d\n",F,L);
     printf("Swap without using 3rd variable is: %d,%d",f,l);
-
-
 return 0;
 }
 
 //Problem NO:03;
 #include <stdio.h>
-
 int main(){
     printf("Enter three value:");
     float x,y,z;
     scanf("%f%f%f",&x,&y,&z);
     int t = x+y+z;
     printf("Total= %dtk",t);
-
-
 return 0;
 }
